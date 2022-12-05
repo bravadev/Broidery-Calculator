a6c79f956d79ae32c28401a1d1f1eabfb6e37f7f
Tugdual <harel.tugdual@gmail.com>
2022-12-05 23:13:55 +0100

    test

--- app.js
+++ app.js
@@ -1,38 +1,74 @@
+// Récupération du formulaire
+var form = document.querySelector("form");
 // Récupération des éléments du formulaire
-const form = document.getElementById('broderie-form');
-const zoneWidthInput = document.getElementById('zone-width');
-const zoneHeightInput = document.getElementById('zone-height');
-const motifWidthInput = document.getElementById('motif-width');
-const motifHeightInput = document.getElementById('motif-height');
+var broderieWidthInput = document.querySelector("#broderieWidth");
+var broderieHeightInput = document.querySelector("#broderieHeight");
+var motifOrPrenomSelect = document.querySelector("#motifOrPrenom");
+var motifInput = document.querySelector("#motif");
+var motifWidthInput = document.querySelector("#motifWidth");
+var motifHeightInput = document.querySelector("#motifHeight");
+var prenomInput = document.querySelector("#prenom");
+var prenomWidthInput = document.querySelector("#prenomWidth");
+var prenomHeightInput = document.querySelector("#prenomHeight");
+var spacingInput = document.querySelector("#spacing");
 
-// Récupération des éléments pour afficher les résultats
-const resultsElement = document.getElementById('results');
-const marginWidthElement = document.getElementById('margin-width');
-const marginHeightElement = document.getElementById('margin-height');
-
-// Fonction pour calculer les marges
-function calculateMargins() {
-    // Récupération des valeurs des champs de formulaire
-    const zoneWidth = zoneWidthInput.value;
-    const zoneHeight = zoneHeightInput.value;
-    const motifWidth = motifWidthInput.value;
-    const motifHeight = motifHeightInput.value;
-
-    // Calcul des marges
-    const marginWidth = (zoneWidth - motifWidth) / 2;
-    const marginHeight = (zoneHeight - motifHeight) / 2;
-
-    // Affichage des résultats
-    resultsElement.style.display = 'block';
-    marginWidthElement.innerHTML = marginWidth;
-    marginHeightElement.innerHTML = marginHeight;
+// Fonction pour calculer les marges en largeur et en hauteur
+function calculateMargins(broderieWidth, broderieHeight, motifWidth, motifHeight, prenomWidth, prenomHeight, spacing) {
+    // Calcul des marges en largeur
+    var broderieWidthMargin = broderieWidth - (motifWidth * spacing) - prenomWidth;
+    // Calcul des marges en hauteur
+    var broderieHeightMargin = broderieHeight - (motifHeight * spacing) - prenomHeight;
+    // Affichage des marges en largeur et en hauteur
+    alert("Marges en largeur: " + broderieWidthMargin + "\nMarges en hauteur: " + broderieHeightMargin);
 }
 
-// Gestionnaire d'événements pour la soumission du formulaire
-form.addEventListener('submit', (event) => {
-    // Empêche la page de se rafraîchir
-    event.preventDefault();
+// Écouteur d'événement pour capturer la soumission du formulaire
+form.addEventListener("submit", function (e) {
+    // Empêcher l'envoi du formulaire
+    e.preventDefault();
 
-    // Calcule et affiche les marges
-    calculateMargins();
+    // Récupération des valeurs des inputs
+    var broderieWidth = broderieWidthInput.value;
+    var broderieHeight = broderieHeightInput.value;
+    var motifOrPrenom = motifOrPrenomSelect.value;
+    var motif = motifInput.value;
+    var motifWidth = motifWidthInput.value;
+    var motifHeight = motifHeightInput.value;
+    var prenom = prenomInput.value;
+    var prenomWidth = prenomWidthInput.value;
+    var prenomHeight = prenomHeightInput.value;
+    var spacing = spacingInput.value;
+
+    // Si l'utilisateur a choisi de broder un motif seul
+    if (motifOrPrenom == "motif") {
+        // Calcul des marges en largeur et en hauteur
+        calculateMargins(broderieWidth, broderieHeight, motifWidth, motifHeight, 0, 0, spacing);
+    }
+    // Si l'utilisateur a choisi de broder un prénom et un motif
+    else if (motifOrPrenom == "prenom") {
+        // Calcul des marges en largeur et en hauteur
+        calculateMargins(broderieWidth, broderieHeight, motifWidth, motifHeight, prenomWidth, prenomHeight, spacing);
+    }
+    // Si l'utilisateur n'a pas choisi de broder un motif seul ou un prénom et un motif
+    else {
+        alert("Vous devez choisir de broder un motif seul ou un prénom et un motif!");
+    }
+});
+
+// Écouteur d'événement pour afficher/cacher les inputs liés au prénom en fonction du choix de l'utilisateur
+motifOrPrenomSelect.addEventListener("change", function () {
+    // Si l'utilisateur a choisi de broder un motif seul
+    if (this.value == "motif") {
+        // Masquer les inputs liés au prénom
+        prenomInput.style.display = "none";
+        prenomWidthInput.style.display = "none";
+        prenomHeightInput.style.display = "none";
+    }
+    // Si l'utilisateur a choisi de broder un prénom et un motif
+    else if (this.value == "prenom") {
+        // Afficher les inputs liés au prénom
+        prenomInput.style.display = "block";
+        prenomWidthInput.style.display = "block";
+        prenomHeightInput.style.display = "block";
+    }
 });
